853ffc1a06058675b44572c2bc25ff3c43cd46dc
ha-rshh <prajapati.captain@gmail.com>
2025-02-16 17:37:55 +0530

    array machine coding

--- JavaScript/W4.2_JS_LoopsAndArrays.js
+++ JavaScript/W4.2_JS_LoopsAndArrays.js
@@ -1,14 +1,6 @@
 // ARRAYS
 
-let fruits = ["apple", "mango", "cherry"];
-let intFruits = ["Kiwi", "Avacado", "Dragon Fruit"];
-
-// console.log(fruits);
-// console.log(intFruits);
-
-// console.log(typeof fruits);
-// console.log(typeof intFruits);
-
+let fruits = ["apple", "banana", "cherry"];
 fruits[0] = "blueberry";
 
 console.log(fruits[0]);
@@ -33,10 +25,9 @@ console.log(fruits);
 // step - 2 - limit batao
 // step - 3 - incre ya decre = h = h + 1; this is also same h++
 
-for (let h = 0; h < teas.length + 99; h++) {
-  console.log(`Teas at index ${h}: ${teas[h]}`);
-}
-
+// for (let h = 0; h < teas.length + 99; h++) {
+//   console.log(`Teas at index ${h}: ${teas[h]}`);
+// }
 
 // sum of number in an array
 
@@ -56,3 +47,75 @@ console.log(result);
 
 let anotherArray = sumFactor([3, 6, 9, 3, 7, 3, 7, 6]);
 console.log(`My result of anotherArrays: ${anotherArray}`);
+
+// * ARRAY MACHINE CODING
+
+// Problem: Create an array containing different types of teas.
+
+const teas = [
+  "Green Tea",
+  "Black Tea",
+  "Oolong Tea",
+  "White Tea",
+  "Herbal Tea",
+];
+
+console.log(teas);
+
+// Add "Chamomile Tea" to the existing list of teas.
+teas.push("Chamomile Tea");
+console.log(teas);
+
+// Remove Oolong Tea from the list of teas.
+const index = teas.indexOf("Oolong Tea");
+console.log(teas.indexOf("Oolong Tea"));
+if (index > -1) {
+  teas.splice(index, 1);
+}
+console.log(teas);
+// filter the list to only include teas that are caffienated. (herbal teas isn't cafienated)
+const caffienatedTeas = teas.filter((tea) => tea !== "Herbal Tea");
+console.log(caffienatedTeas);
+
+// Sort the list of teas in alphabetical order.
+console.log(teas.sort());
+
+// use a loop to pritn each type of tea in the array
+
+for (let i = 0; i < teas.length; i++) {
+  console.log(teas[i]);
+}
+
+// Use a for loop to count how many teas are caffeinated ("excluding Hearbal Tea")
+let caffTeas = [];
+let count = 0
+for (let i = 0; i < teas.length; i++) {
+  if (teas[i] !== "Herbal Tea") {
+    caffTeas.push(teas[i]);
+    count++
+  }
+}
+console.log(caffTeas, count);
+// use a for loop to create a new array with all tea names is uppercase.
+
+const upperCase = []
+for(let i = 0; i < teas.length ; i++){
+  upperCase.push(teas[i].toUpperCase())
+}
+console.log(upperCase)
+
+//! Use a for loop ot find tea name with the most characters.
+
+let longestTea = ""
+for( let i = 0; i< teas.length ; i++){
+  if(teas[i].length > longestTea.length){
+    longestTea = teas[i]
+  }
+}
+
+// use a for loop to reverse the order of teas in the array
+let newArr = []
+for(let i = teas.length - 1; i >= 0; i--){
+    newArr.push(teas[i])
+}
+console.log(newArr)
